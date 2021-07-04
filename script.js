a9671c67dbef4a1215a758eb017cd4c7cde18e01
Kamal <72544060+Syed-Mohtesham-Kamal@users.noreply.github.com>
2021-07-04 12:08:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
-// const canvas = document.querySelector('canvas');
-// const ctx = canvas.getContext('2d');
-// let isDrawing = false;
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+let isDrawing = false;
 
-// canvas.addEventListener('mousedown',start);
-// canvas.addEventListener('mousemove',draw);
-// canvas.addEventListener('mouseup',stop);
+canvas.addEventListener('mousedown',start);
+canvas.addEventListener('mousemove',draw);
+canvas.addEventListener('mouseup',stop);
 
-// function stop(e){
-//   isDrawing = true;
-// }
+function stop(e){
+isDrawing = true;
+}
 
-// function draw({clientX: c, clientY: y}){
-//   if(!isDrawing) return;
+function draw({clientX: c, clientY: y}){
+if(!isDrawing) return;
 
-//   ctx.lineWidth = 3;
-//   ctx.lineCap = "round";
-//   ctx.strokeStyle = "#171717";
+ctx.lineWidth = 3;
+ctx.lineCap = "round";
+ctx.strokeStyle = "#171717";
 
-//   ctx.lineTo(x,y);
-//   ctx.stroke();
-//   ctx.beginPath();
-//   ctx.moveTo(x,y);
-// }
+ctx.lineTo(x,y);
+ctx.stroke();
+ctx.beginPath();
+ctx.moveTo(x,y);
+}
 
-// function stop (){
-//   isDrawing = flase;
-//   ctx.beginPath();
-// }
+function stop (){
+isDrawing = flase;
+ctx.beginPath();
+}
 
-// window.addEventListener('resize', resizeCanvas);
-// function resizeCanvas(){
-//   canvas.width = window.innerWidth;
-//   canvas.height = window.innerWidth;
-// }
-// resizeCanvas();
+window.addEventListener('resize', resizeCanvas);
+function resizeCanvas(){
+canvas.width = window.innerWidth;
+canvas.height = window.innerWidth;
+}
+resizeCanvas();
